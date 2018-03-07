ba9a70abf06ffaa60436e58a9504a2900a61901e
shiopon01 <shio.0323@gmail.com>
2018-03-07 17:26:24 +0900

    CSS in JS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+// import './App.css' // Possible to import css :)
 
 // containers
 import { AddTodo, VisibleTodoList } from './containers'
@@ -11,12 +11,12 @@ import { Footer } from './components'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React/Redux starter kit</h1>
+      <div style={style.app}>
+        <header style={style.appHeader}>
+          <img src={logo} style={style.appLogo} alt="logo" />
+          <h1 style={style.appTitle}>Welcome to React/Redux starter kit</h1>
         </header>
-        <p className="App-intro">
+        <p style={style.appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
@@ -27,6 +27,32 @@ class App extends Component {
       </div>
     );
   }
+}
+
+let style = {
+  app: {
+    textAlign: 'center'
+  },
+
+  appLogo: {
+    animation: 'App-logo-spin infinite 20s linear',
+    height: '80px'
+  },
+
+  appHeader: {
+    backgroundColor: '#222',
+    height: '150px',
+    padding: '20px',
+    color: 'white'
+  },
+  
+  appTitle: {
+    fontSize: '1.5em'
+  },
+  
+  appIntro: {
+    fontSize: 'large'
+  },
 }
 
 export default App;
