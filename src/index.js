f8929c7e58885109f339e1a83d531f16ff15b7f4
yorori <>
2018-07-22 16:19:32 +0900

    fix: Import 'applyMiddleware' to resolve the undefined.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { Provider } from 'react-redux'
